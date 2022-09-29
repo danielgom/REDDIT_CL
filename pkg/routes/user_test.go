410cc029823af02b500e0b60196b6df656b2dfc8
DanielGA <dga_355@hotmail.com>
2022-09-28 21:25:02 -0500

    Complete user route tests

--- pkg/routes/user_test.go
+++ pkg/routes/user_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"RD-Clone-API/pkg/internal"
 	"RD-Clone-API/pkg/routes/mock_services"
@@ -17,6 +18,7 @@ import (
 )
 
 var errSign = fmt.Errorf("could not sign up")
+var errVerifyDB = fmt.Errorf("could not verify account")
 
 func TestUserHandler(t *testing.T) {
 	t.Parallel()
@@ -25,6 +27,11 @@ func TestUserHandler(t *testing.T) {
 		"test create user bad request fails":    testCreateUserBadJSON,
 		"test create user invalid fields fails": testCreateUserValidation,
 		"test create user service err fails ":   testCreateUserSvcErr,
+		"test verify account success":           testVerifyAccount,
+		"test verify account service err fails": testVerifyAccountSvcErr,
+		"test login success":                    testLogin,
+		"test login service err fails":          testLoginSvcErr,
+		"test login bad request fails":          testLoginBadJSON,
 	} {
 		fn := fn
 		t.Run(scenario, func(t *testing.T) {
@@ -141,4 +148,119 @@ func testCreateUserSvcErr(t *testing.T, h *UserHandler, svc *mock_services.MockU
 	require.NoError(t, err)
 	require.Equal(t, http.StatusBadRequest, rec.Code)
 	require.Contains(t, rec.Body.String(), "could not sign up")
+}
+
+func testVerifyAccount(t *testing.T, h *UserHandler, svc *mock_services.MockUserService) {
+	t.Helper()
+	e := echo.New()
+
+	req := httptest.NewRequest(http.MethodGet, "/accountVerification/10", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	svc.EXPECT().VerifyAccount(c.Request().Context(), gomock.Any()).Return(nil)
+
+	err := h.VerifyAccount(c)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, rec.Code)
+	require.Contains(t, rec.Body.String(), "Account has been successfully validated!")
+}
+
+func testVerifyAccountSvcErr(t *testing.T, h *UserHandler, svc *mock_services.MockUserService) {
+	t.Helper()
+	e := echo.New()
+
+	req := httptest.NewRequest(http.MethodGet, "/accountVerification/10", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	svc.EXPECT().VerifyAccount(c.Request().Context(), gomock.Any()).
+		Return(errors.NewInternalServerError("database error", errVerifyDB))
+
+	err := h.VerifyAccount(c)
+	require.NoError(t, err)
+	require.Contains(t, rec.Body.String(), "database error")
+	require.Equal(t, http.StatusInternalServerError, rec.Code)
+}
+
+func testLogin(t *testing.T, h *UserHandler, svc *mock_services.MockUserService) {
+	t.Helper()
+	e := echo.New()
+
+	lr := internal.LoginRequest{
+		UserOrEmail: "dga_355@hotmail.com",
+		Password:    "Test12345@",
+	}
+
+	userJSON, err := json.Marshal(lr)
+	require.NoError(t, err)
+
+	req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(string(userJSON)))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	want := &internal.LoginResponse{
+		Username:     "DanielGA",
+		Email:        lr.UserOrEmail,
+		Token:        "jsontoken123",
+		RefreshToken: "jsonrefresh123",
+		ExpiresAt:    time.Now(),
+	}
+
+	svc.EXPECT().Login(c.Request().Context(), &lr).Return(want, nil)
+
+	err = h.Login(c)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusCreated, rec.Code)
+	require.NoError(t, err)
+
+	responseString := rec.Body.String()
+
+	require.Contains(t, responseString, want.Email)
+	require.Contains(t, responseString, want.Username)
+	require.Contains(t, responseString, want.Token)
+	require.Contains(t, responseString, want.RefreshToken)
+}
+
+func testLoginSvcErr(t *testing.T, h *UserHandler, svc *mock_services.MockUserService) {
+	t.Helper()
+	e := echo.New()
+
+	lr := internal.LoginRequest{
+		UserOrEmail: "dga_355@hotmail.com",
+		Password:    "Test12345@",
+	}
+
+	userJSON, err := json.Marshal(lr)
+	require.NoError(t, err)
+
+	req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(string(userJSON)))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	svc.EXPECT().Login(c.Request().Context(), &lr).Return(nil, errors.NewUnauthorisedError("service error"))
+
+	err = h.Login(c)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusUnauthorized, rec.Code)
+	require.Contains(t, rec.Body.String(), "service error")
+}
+
+func testLoginBadJSON(t *testing.T, h *UserHandler, _ *mock_services.MockUserService) {
+	t.Helper()
+	e := echo.New()
+
+	req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader("123123{}"))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	err := h.Login(c)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusBadRequest, rec.Code)
+	require.Contains(t, rec.Body.String(), "invalid json format")
 }
